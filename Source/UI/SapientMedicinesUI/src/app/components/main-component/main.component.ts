f754e4ecab79285e6171bde14dbb4228a520f74c
kathiravan B <itsmekathi92@gmail.com>
2020-09-21 11:15:35 +0530

    closes #2

--- Source/UI/SapientMedicinesUI/src/app/components/main-component/main.component.ts
+++ Source/UI/SapientMedicinesUI/src/app/components/main-component/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MedicineService } from 'src/app/services/medicine.service';
 import { MedicineModel } from 'src/app/models/medicine.model';
+import { cloneDeep, filter } from 'lodash';
 
 @Component({
   selector: 'app-main',
@@ -9,11 +10,18 @@ import { MedicineModel } from 'src/app/models/medicine.model';
 })
 export class MainComponent implements OnInit {
   medicines: Array<MedicineModel> = [];
+  filteredMedicines: Array<MedicineModel> = [];
+
   constructor(private medicineService: MedicineService) {}
   async ngOnInit(): Promise<void> {
     this.medicines = await this.medicineService.getAllMedicines();
+    this.filteredMedicines = cloneDeep(this.medicines);
   }
-  searchMedicines($event): void {
-    console.log($event);
+  searchMedicines($event: string): void {
+    this.filteredMedicines = cloneDeep(
+      this.medicines.filter(
+        (m) => m.fullName.toLowerCase().indexOf($event.toLowerCase()) !== -1
+      )
+    );
   }
 }
